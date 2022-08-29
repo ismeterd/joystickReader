db2462aaa86b178e0c241a36383c5221c696b95b
ismet <erdoganis20@itu.edu.tr>
2022-08-29 22:55:09 +0300

    Created the view_joystick_details(...) function.

--- joystick.py
+++ joystick.py
@@ -27,3 +27,15 @@ class Joystick:
         self.hat = []
         for i in range(self.numhats):
             self.hat.append(self.joy.get_hat(i))
+
+    def view_joystick_details(self):
+        print("\n----Joystick Details----")
+        print(f"Joystick id : {self.id}")
+        print(f"Joystick name: {self.name}")
+        print()
+        print(f"number of axis -> {self.numeaxes}")
+        print(f"number of ball -> {self.numballs}")
+        print(f"number of button -> {self.numbuttons}")
+        print(f"number of hats -> {self.numhats}")
+        print("~~~~~~~~~~~~~~~~~~~~~~~~")
+
