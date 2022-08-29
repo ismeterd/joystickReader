69e2a72a2f925c4c7b9927e978a23a91de732b7f
ismet <erdoganis20@itu.edu.tr>
2022-08-30 00:09:10 +0300

    Added the quit() function and imported sys module.

--- joystick_test.py
+++ joystick_test.py
@@ -1,3 +1,5 @@
+import sys
+
 import pygame
 from joystick import Joystick
 
@@ -12,10 +14,16 @@ class InputTest:
         if self.joycount == 0:
             print("\nNo joysticks were detected!")
             # to be exited the program with code 1.
+            self.quit(1)
         else:
             print(f"\nDetected {self.joycount} joystick/joysticks")
             for i in range(self.joycount):
                 self.joys.append(Joystick(i))
+
+    def quit(self, status=0):
+        print("Program is ending...")
+        pygame.quit()
+        sys.exit(status)
 
 
 if __name__ == "__main__":
